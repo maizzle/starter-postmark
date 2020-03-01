e036c08dc6fc13c07121aacefed6879928aac588
Cosmin Popovici <hellocosmin@gmail.com>
2020-03-01 13:23:29 +0200

    Remove all: screen.

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,7 +4,6 @@ module.exports = {
   separator: ':',
   theme: {
     screens: {
-      all: {'raw': 'screen'},
       sm: {'max': '600px'},
       dark: {'raw': '(prefers-color-scheme: dark)'},
     },
